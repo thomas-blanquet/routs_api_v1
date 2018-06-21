6dccce1f523ffd09c22f45556f92987b23187f8c
Thomas Blanquet <thomas.blanquet@epitech.eu>
2018-06-21 14:33:56 +0200

    hello world

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = {
+  hello: (name) => {
+    return 'Hello !';
+  },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql:true,
+}));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
