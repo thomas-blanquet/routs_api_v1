6f4978a48bbc80b0496b3c12bfa8585a2a33a37d
Thomas Blanquet <thomas.blanquet@epitech.eu>
2018-06-22 10:35:05 +0200

    test basic types

--- server.js
+++ server.js
@@ -7,13 +7,25 @@ var port = process.env.PORT || 8080;
 var schema = buildSchema(`
   type Query {
     hello: String
+    quote: String
+    random: Float!
+    rollTreeDices: [Int]
   }
 `);
 
 var root = {
-  hello: () => {
-    return 'Hello !';
-  },
+    hello: () => {
+	return 'Hello !';
+    },
+    quote: () => {
+	return Math.random() > 0.5 ? "It is what it is" : "Shit happens";
+    }
+    random: () => {
+	return Math.random();
+    }
+    rollThreeDices: () => {
+	return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+    }
 };
 
 var app = express();
